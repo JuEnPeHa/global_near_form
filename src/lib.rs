6c04db4ee9044a5113b73466225b6b3bc3e57f91
JuEnPeHa <jeph@nearlearning.app>
2022-05-28 21:36:56 -0500

    global_near_form_second_commit

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use near_sdk::json_types::{U128, U64};
 use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{ promise_result_as_success,
     assert_one_yocto, env, ext_contract, near_bindgen, AccountId, Balance,
-    Gas, PanicOnDefault, Promise, CryptoHash, BorshStorageKey,
+    Gas, PanicOnDefault, Promise, CryptoHash, BorshStorageKey, require,
 };
 
 trait AccountIdExt {
@@ -13,13 +13,13 @@ trait AccountIdExt {
 
 impl AccountIdExt for AccountId {
     fn is_zero(&self) -> bool {
-        self.is_zero()
+        self.as_bytes().iter().all(|b| *b == 0)
     }
 }
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct Account {
+pub struct StudentAccount {
     pub balance: Balance,
     pub nonce: U64,
     pub creation_height: U64,
@@ -27,7 +27,7 @@ pub struct Account {
 
 #[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
 #[serde(crate = "near_sdk::serde")]
-pub struct Answer {
+pub struct AnswerNIT {
     pub nonce: U64,
     pub answer_friday_class: u8,
     pub friday_professor: AccountId,
@@ -39,19 +39,58 @@ pub struct Answer {
     pub wednesday_professor: AccountId,
     pub answer_thursday_class: u8,
     pub thursday_professor: AccountId,
-    
+
+    pub answer_nit_main_class: u8,
+    pub nit_main_professor: AccountId,
+
+}
+
+#[derive(BorshDeserialize, BorshSerialize, Serialize, Deserialize)]
+#[serde(crate = "near_sdk::serde")]
+pub struct QuestionNIT {
+    pub nonce: String,
+    pub question_friday_class: String,
+    pub friday_professor: Vec<AccountId>,
+    pub question_monday_class: String,
+    pub monday_professor: Vec<AccountId>,
+    pub question_tuesday_class: String,
+    pub tuesday_professor: Vec<AccountId>,
+    pub question_wednesday_class: String,
+    pub wednesday_professor: Vec<AccountId>,
+    pub question_thursday_class: String,
+    pub thursday_professor: Vec<AccountId>,
+
+    pub question_nit_main_class: String,
+    pub nit_main_professor: Vec<AccountId>,
 
 }
 
 #[near_bindgen]
 #[derive(BorshDeserialize, BorshSerialize, PanicOnDefault)]
 pub struct Contract {
-    pub by_account_id: LookupMap<AccountId, Account>,
-    pub accounts_already_answered_form: UnorderedSet<AccountId>,
-
+    pub owner_id: AccountId,
+    pub by_account_id: LookupMap<AccountId, StudentAccount>,
+    pub accounts_already_answered_nit: UnorderedSet<AccountId>,
+    pub storage_deposits: UnorderedMap<AccountId, Balance>,
 }
 
 #[near_bindgen]
 impl Contract {
+    #[init]
+    pub fn new_default_meta() -> Self {
+        let owner_id = env::signer_account_id();
+        Self::new(
+            owner_id,
+        )
+    }
 
+    #[init]
+    pub fn new(owner_id: AccountId) -> Self {
+        require!(!env::state_exists(), "Contract already initialized");
+        let this = Self {
+            owner_id,
+
+        };
+        this
+    }
 }
